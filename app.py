97fd7f57ffa4fb5935f969d53a358084f3e5eb0d
Thaddeus Toledo <toledothaddeusf@gmail.com>
2023-07-23 02:18:20 +0800

    Improved user priviledges

--- app.py
+++ app.py
@@ -35,30 +35,12 @@ if os.path.isfile('data/users.json') == False:
     username = input('Enter a username: ')
     password = getpass.getpass('Enter a password: ')
     hashed_password = hash_password(password)
-    getDb('data/users.json').add({'username': username, 'password': hashed_password['password'], 'salt': hashed_password['salt'], 'authorized_tbl': []})
+    getDb('data/users.json').add({'username': username, 'password': hashed_password['password'], 'salt': hashed_password['salt'], 'permissions': {}})
 
 app = Flask(__name__)
 
 class UserHandler:
-    def deauthorize_user(self, username, table_name):
-        data = getDb('data/users.json').getByQuery({'username': username})
-        if len(data) == 0:
-            return {
-                'status': 'error',
-                'message': 'User not found'
-            }
-        if table_name not in data[0]['authorized_tbl']:
-            return {
-                'status': 'error',
-                'message': f'User not authorized to table {table_name}'
-            }
-        data[0]['authorized_tbl'].remove(table_name)
-        getDb('data/users.json').updateById(data[0]['id'], {'authorized_tbl': data[0]['authorized_tbl']})
-        return {
-            'status': 'success',
-            'message': f'User deauthorized from table {table_name}'
-        }
-    def authorize_user(self, username, table_name):
+    def change_permission(self, permission, table_name, username):
         data = getDb('data/users.json').getByQuery({'username': username})
         if len(data) == 0:
             return {
@@ -70,17 +52,28 @@ class UserHandler:
                 'status': 'error',
                 'message': 'Table not found'
             }
-        if table_name in data[0]['authorized_tbl']:
+        if permission == 'none':
+            data[0]['permissions'].pop(table_name)
+            getDb('data/users.json').updateById(data[0]['id'], {'permissions': data[0]['permissions']})
             return {
-                'status': 'error',
-                'message': f'User already authorized to table {table_name}'
+                'status': 'success',
+                'message': f'Permission removed for {username} on {table_name}'
             }
-        data[0]['authorized_tbl'].append(table_name)
-        getDb('data/users.json').updateById(data[0]['id'], {'authorized_tbl': data[0]['authorized_tbl']})
+        allowed_permissions = ['a', 'r', 'w']
+        cleaned_permissions = list(set(permission))
+        for x in cleaned_permissions:
+            if x not in allowed_permissions:
+                return {
+                    'status': 'error',
+                    'message': 'Invalid permission'
+                }
+        data[0]['permissions'][table_name] = cleaned_permissions
+        getDb('data/users.json').updateById(data[0]['id'], {'permissions': data[0]['permissions']})
         return {
             'status': 'success',
-            'message': f'User authorized to table {table_name}'
+            'message': f'Permission changed to {cleaned_permissions} for {username} on {table_name}'
         }
+
     
     def check_user_validity(self, username, password):
         data = getDb('data/users.json').getByQuery({'username': username})
@@ -91,13 +84,48 @@ class UserHandler:
         salted_password = password + salt
         return hashlib.sha256(salted_password.encode()).hexdigest() == hashed_password
     
-    def check_table_authorization(self, table, username):
+    def check_table_permission(self, permission, table, username):
         data = getDb('data/users.json').getByQuery({'username': username})
         if len(data) == 0:
-            return False
-        if table not in data[0]['authorized_tbl']:
-            return False
-        return True
+            return {
+                'status': 'error',
+                'message': 'User not found'
+            }
+        if table not in TableHandler().list_tables():
+            return {
+                'status': 'error',
+                'message': 'Table not found'
+            }
+        if table not in data[0]['permissions']:
+            return {
+                'status': 'error',
+                'message': 'You have no permissions to this table'
+            }
+        if permission not in data[0]['permissions'][table]:
+            if permission == 'a':
+                return {
+                    'status': 'error',
+                    'message': 'You do not have admin permissions to this table'
+                }
+            elif permission == 'r':
+                return {
+                    'status': 'error',
+                    'message': 'You do not have read permissions to this table'
+                }
+            elif permission == 'w':
+                return {
+                    'status': 'error',
+                    'message': 'You do not have write permissions to this table'
+                }
+            else:
+                return {
+                    'status': 'error',
+                    'message': 'Invalid permission'
+                }
+        return {
+            'status': 'success',
+            'message': 'You have permissions to this table'
+        }
     
     def create_user(self, username, password):
         hashed_password = hash_password(password)
@@ -106,7 +134,13 @@ class UserHandler:
                 'status': 'error',
                 'message': 'Username already exists'
             }
-        getDb('data/users.json').add({'username': username, 'password': hashed_password['password'], 'salt': hashed_password['salt'], 'authorized_tbl': []})
+        query = {
+            'username': username,
+            'password': hashed_password['password'],
+            'salt': hashed_password['salt'],
+            'permissions': {}
+        }
+        getDb('data/users.json').add(query)
         return {
             'status': 'success',
             'message': 'User created'
@@ -124,6 +158,8 @@ class UserHandler:
             'message': 'User deleted'
         }
 
+
+
 class TableHandler:
     def list_tables(self):
         data = os.listdir('tables')
@@ -133,7 +169,7 @@ class TableHandler:
                 files.append(file[:-5])  
         return files
     
-    def create_table(self, table_name):
+    def create_table(self, table_name, username = 'system'):
         if table_name in self.list_tables():
             return {
                 'status': 'error',
@@ -146,6 +182,8 @@ class TableHandler:
                 'status': 'error',
                 'message': 'Invalid table name'
             }
+        if username != 'system':
+            UserHandler().change_permission('rwa', table_name, username)
         return {
             'status':'success',
             'message': f'Table {table_name} created',
@@ -314,21 +352,17 @@ adduser - <username> <password> - add a user to the database
 deluser - <username> - delete a user from the database
 update - <tbl_name> <search_query:json> <update_data:json> - update data in a table
 clear - clear the console
-authorize - <tbl_name> <username> - authorize a user to a table
-deauthorize - <tbl_name> <username> - deauthorize a user from a table                   
+permissions - <permission> <tbl_name> <username> - authorize a user to a table, Permissions: r - read, w - write, rw - read and write           
                 ''')      
             elif splitted_command[0] == 'delete':
                 tbl_name = splitted_command[1]
                 search_query = re.findall(r'{.*?}', command)[0]
                 print(TableHandler().delete_data(tbl_name, ast.literal_eval(search_query)))
-            elif splitted_command[0] == 'deauthorize':
-                tbl_name = splitted_command[1]
-                username = splitted_command[2]
-                print(UserHandler().deauthorize_user(username, tbl_name))
-            elif splitted_command[0] == 'authorize':
-                tbl_name = splitted_command[1]
-                username = splitted_command[2]
-                print(UserHandler().authorize_user(username, tbl_name))
+            elif splitted_command[0] == 'permissions':
+                permission = splitted_command[1]
+                tbl_name = splitted_command[2]
+                username = splitted_command[3]
+                print(UserHandler().change_permission(permission, tbl_name, username))
             elif splitted_command[0] == 'update':
                 tbl_name = splitted_command[1]
                 search_query = re.findall(r'{.*?}', command)[0]
@@ -396,18 +430,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return jsonify({'status': 'error','message': 'Invalid request or missing fields'})
 
-@app.route('/remove', methods=["POST"])
-def remove():
-    required = ['tbl_name', 'search_query', 'username', 'password']
-    for field in required:
-        if field not in request.form:
-            return jsonify({'status': 'error','message': f"Missing field {field}"})
-    if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
-        return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('username'), request.form.get('tbl_name')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
-    return TableHandler().delete_data(request.form.get('tbl_name'), ast.literal_eval(request.form.get('search_query')))
-
 @app.route('/update', methods=["POST"])
 def update():
     required = ['tbl_name', 'data', 'search_query', 'username', 'password']
@@ -416,8 +438,9 @@ def update():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('tbl_name'), request.form.get('username')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
+    permission_status = UserHandler().check_table_permission('w', request.form.get('tbl_name'), request.form.get('username'))
+    if permission_status['status'] == 'error':
+        return jsonify(permission_status)
     return TableHandler().update_data(request.form.get('tbl_name'), ast.literal_eval(request.form.get('search_query')), ast.literal_eval(request.form.get('data')))
     
 @app.route('/search', methods=["POST"])
@@ -428,8 +451,9 @@ def search():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('tbl_name'), request.form.get('username')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
+    permission_status = UserHandler().check_table_permission('r', request.form.get('tbl_name'), request.form.get('username'))
+    if permission_status['status'] == 'error':
+        return jsonify(permission_status)
     if request.form.get('search_query') == '*' or request.form.get('search_query') == 'all':
         return TableHandler().search_data(request.form.get('tbl_name'), request.form.get('search_query'))
     return TableHandler().search_data(request.form.get('tbl_name'), ast.literal_eval(request.form.get('search_query')))
@@ -442,8 +466,9 @@ def insert():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('tbl_name'), request.form.get('username')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
+    permission_status = UserHandler().check_table_permission('w', request.form.get('tbl_name'), request.form.get('username'))
+    if permission_status['status'] == 'error':
+        return jsonify(permission_status)
     return TableHandler().insert_data(request.form.get('tbl_name'), ast.literal_eval(request.form.get('data')))
     
 @app.route('/delete', methods=["POST"])
@@ -454,8 +479,9 @@ def delete():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('tbl_name'), request.form.get('username')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
+    permission_status = UserHandler().check_table_permission('w', request.form.get('tbl_name'), request.form.get('username'))
+    if permission_status['status'] == 'error':
+        return jsonify(permission_status)
     try:
         query = ast.literal_eval(request.form.get('search_query'))
     except ValueError:
@@ -470,8 +496,10 @@ def drop():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    if UserHandler().check_table_authorization(request.form.get('tbl_name'), request.form.get('username')) == False:
-        return jsonify({'status': 'error','message': 'You are not authorized to this table'})
+    permission_status = UserHandler().check_table_permission('a', request.form.get('tbl_name'), request.form.get('username'))
+    if permission_status['status'] == 'error':
+        return jsonify(permission_status)
+    UserHandler().change_permission('none', request.form.get('tbl_name'), request.form.get('username'))
     return TableHandler().delete_table(request.form.get('tbl_name'))
     
 @app.route('/create', methods=["POST"])
@@ -482,7 +510,7 @@ def create():
             return jsonify({'status': 'error','message': f"Missing field {field}"})
     if UserHandler().check_user_validity(request.form.get('username'), request.form.get('password')) == False:
         return jsonify({'status': 'error','message': 'Invalid username or password'})
-    return TableHandler().create_table(request.form.get('tbl_name'))
+    return TableHandler().create_table(request.form.get('tbl_name'), username = request.form.get('username'))
     
 @app.route('/list', methods=["POST"])
 def list_tbls():
